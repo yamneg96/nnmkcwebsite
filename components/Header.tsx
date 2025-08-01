abfb5fc132dd8580d144e3b57c5d0de6e6b231fa
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-02 01:42:25 +0300

    Layout Update

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Header = () => {
+  return (
+      <Head>
+        <title>NNMKC - Welcome Home</title>
+        <meta name="description" content="Join our loving community at NNMKC. Experience faith, fellowship, and spiritual growth." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/MKC-Logo.png" />
+      </Head>
+  )
+}
+
+export default Header
