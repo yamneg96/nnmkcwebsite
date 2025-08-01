4004497c3544d411e8d3733e1d2f6a7b168ea97b
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-02 01:42:54 +0300

    Applied updated layout

--- pages/BibleStudy/BibleStudy.tsx
+++ pages/BibleStudy/BibleStudy.tsx
@@ -1,10 +1,26 @@
+import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 import React from 'react'
 
 const BibleStudy = () => {
   return (
-    <div className='text-4xl font-bold text-center mt-6'>
-      Bible Study Program
-    </div>
+    <section className='min-h-screen'>
+      <Navbar />
+      <Header />
+      <main className='min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6 text-black'>
+        <div className='text-4xl font-bold text-center mt-25'>
+          Bible Study Program
+        </div>
+        <div className='text-center mt-10'>
+          <p className='text-lg mb-6'>
+            Join us for our weekly Bible study sessions where we delve into the Word of God, 
+            explore its teachings, and grow together in faith. Everyone is welcome!
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </section>
   )
 }
 
