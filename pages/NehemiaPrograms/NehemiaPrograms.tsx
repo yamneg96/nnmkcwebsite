0a4a9ceb5360aa9e080c56683a9ea7aaa74855a2
Yamlak Negash <yamlaknegash96@gmail.com>
2025-08-02 01:18:15 +0300

    Configured Routing between the pages.

---
+++ pages/NehemiaPrograms/NehemiaPrograms.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NehemiaPrograms = () => {
+  return (
+    <div className='text-4xl font-bold text-center mt-6'>
+      Nehemia Programs
+    </div>
+  )
+}
+
+export default NehemiaPrograms
